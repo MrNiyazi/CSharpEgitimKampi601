309c92bb01356dad3386ac283be0f53d952a9a55
Berat Aktas <berataktas2834@gmail.com>
2024-12-31 23:58:25 +0300

    31.12.2024-1

--- CSharpEgitimKampi601/Services/MongoDbConnection.cs
+++ CSharpEgitimKampi601/Services/MongoDbConnection.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace CSharpEgitimKampi601.Services
 {
-	internal class MongoDbConnection
+	public class MongoDbConnection
 	{
+		private IMongoDatabase _database;
+
+		public MongoDbConnection()
+		{
+			var client = new MongoClient("mongodb://localhost:27017");
+			_database = client.GetDatabase("Db601Customer");
+		}
+
+		public IMongoCollection<BsonDocument> GetCustomersCollection()
+		{
+			return _database.GetCollection<BsonDocument>("Customer");
+		}
+
 	}
 }
